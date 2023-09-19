125479788cbc2d11a0493c85f3c9a227bb7cb726
Daniel Gabriel <revogabe.dev@gmail.com>
2023-09-19 20:25:54 -0300

    init commit: i am happy

---
+++ Translify/TranslifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslifyApp.swift
+//  Translify
+//
+//  Created by Daniel Gabriel on 19/09/23.
+//
+
+import SwiftUI
+
+@main
+struct TranslifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
