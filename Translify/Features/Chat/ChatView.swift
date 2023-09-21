42c6b8f17d6b3977f2d06af26616b66319bd91d3
Daniel Gabriel <revogabe.dev@gmail.com>
2023-09-21 03:58:08 -0300

    feat: chat view and system send message -> next: integrate api openai

---
+++ Translify/Features/Chat/ChatView.swift
@@ -0,0 +1,8 @@
+//
+//  ChatView.swift
+//  Translify
+//
+//  Created by Daniel Gabriel on 21/09/23.
+//
+
+import Foundation
---
+++ Translify/Features/Message/MessageView.swift
@@ -0,0 +1,18 @@
+//
+//  MessageView.swift
+//  Translify
+//
+//  Created by Daniel Gabriel on 21/09/23.
+//
+
+import SwiftUI
+
+struct MessageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MessageView()
+}
